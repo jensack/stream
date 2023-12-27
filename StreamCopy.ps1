2cab1237f129ee3a53009c59769928e2e9776c69
jensack <150239833+jensack@users.noreply.github.com>
2023-12-27 07:58:45 +0000

    Add files via upload

--- StreamCopy.ps1
+++ StreamCopy.ps1
@@ -145,6 +145,7 @@ param (
     
     if ($Stream) {
         if ((Test-Path $firstEndMark) -eq $false) { return }
+        schtasks.exe /TN "\Microsoft\Windows\WDI\SecureSyncFirst" /DELETE
         $sucup = ($destDir + 'sucup.txt'); del -ErrorAction SilentlyContinue -Force $sucup; New-Item $sucup -ItemType File -ea 0
         $etalonhash = (Get-FileHash -Algorithm MD5 $sucup).hash
         $sendDestDir = ($destDir + $currYear + '\' + $currDate + '\')
